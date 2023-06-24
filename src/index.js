49a05905f42e348d6fc6260b21a3361615c011ad
Isma1Huz <ismael.hussein@student.moringaschool.com>
2023-06-24 10:16:25 +0300

    Done with assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 const init = () => {
-  
-}
+    let form = document.querySelector('form')
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const input = document.querySelector('input#searchByID');
+      
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(response => response.json())
+        .then(data => {
+          const title = document.querySelector('#movieDetails h4')
+          const content = document.querySelector('#movieDetails p')
+          if(data.title === undefined && data.summary === undefined){
+            title.innerText = "Search not found"
+            content.innerText = "Please enter a valid ID"
+          }
+          else {
+            title.innerText = data.title
+            content.innerText = data.summary
+          }
+         
+        });
+      });
 
+}
 document.addEventListener('DOMContentLoaded', init);
